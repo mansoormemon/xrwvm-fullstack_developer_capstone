7525d99d76b4126ac13de6ed8b72d59bfad0233e
Mansoor Ahmed Memon <mansoormemon@barakah.works>
2025-12-09 22:33:40 -0500

    module 3: lab 2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,17 +1,26 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
+from django.db import models
+
 # from django.utils.timezone import now
 # from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 
+
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # Other fields as needed
+
+    def __str__(self):
+        return self.name  # Return the name as the string representation
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -23,3 +32,21 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = "SEDAN"
+    SUV = "SUV"
+    WAGON = "WAGON"
+    TYPE_CHOICES = [
+        (SEDAN, "Sedan"),
+        (SUV, "SUV"),
+        (WAGON, "Wagon"),
+        # Add more types as needed
+    ]
+
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} ({self.year})"  # Return name and year as string representation
